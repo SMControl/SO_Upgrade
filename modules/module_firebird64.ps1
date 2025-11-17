708e17505792ba30b5a6cd89f48703ad46be5c8b
SMControl <143865768+SMControl@users.noreply.github.com>
2025-11-17 19:38:12 +0000

    Create module_firebird64.ps1
    initial creation. just copying the 32bit one for now as a placeholder

---
+++ modules/module_firebird64.ps1
@@ -0,0 +1,15 @@
+# module_firebird.ps1
+$installerUrl = "https://raw.githubusercontent.com/SMControl/SO_Upgrade/main/bin/Firebird-4.0.1.exe"
+write-host "Checking if Firebird is Installed"
+if (!(Test-Path "C:\Program Files (x86)\Firebird")) { 
+    $installerPath = "$env:TEMP\Firebird-4.0.1.exe"
+    write-host "Firebird is not installed" -ForegroundColor Red
+    write-host "Obtaining Installer"
+    Invoke-WebRequest -Uri $installerUrl -OutFile $installerPath
+    write-host "Installing Firebird"
+    Start-Process -FilePath $installerPath -ArgumentList "/LANG=en", "/NORESTART", "/VERYSILENT", "/MERGETASKS=UseClassicServerTask,UseServiceTask,CopyFbClientAsGds32Task" -Wait
+    write-host "Editing firebird.conf"
+    (Get-Content "C:\Program Files (x86)\Firebird\Firebird_4_0\firebird.conf") -replace '#DataTypeCompatibility.*', 'DataTypeCompatibility = 3.0' | Set-Content "C:\Program Files (x86)\Firebird\Firebird_4_0\firebird.conf"
+    write-host "Starting Firebird Service"
+    Start-Service -Name "FirebirdServerDefaultInstance"
+}
