5716bd33e32df5ccc5d9e19d467710258c560710
SMControl <143865768+SMControl@users.noreply.github.com>
2025-11-21 11:01:56 +0000

    Update module_firebird64.ps1

--- modules/module_firebird64.ps1
+++ modules/module_firebird64.ps1
@@ -1,43 +1,29 @@
-# Script Version: 1.1.1
-# Module Name: Firebird 64-bit Installer
-# Description: Installs the Firebird SQL Server (64-bit).
-# ----------------------------------------------------------------------------------
-
-# Output the script name and version
-Write-Host "Running module_firebird64.ps1 - Version 1.1.1"
+#Write-Host "Running module_firebird64.ps1 - Version 1.1.1"
 
 $installerUrl = "https://files.stationmaster.info/Firebird-4.0.6.3221-0-x64.exe"
 
-# Configuration content declared as a variable for easy adjustment
 $firebirdConfigContent = @"
+# Essential
 DataTypeCompatibility = 3.0
-DefaultDBCachePages = 16384
-TempCacheLimit = 8000M
+# Safe performance gains
+DefaultDBCachePages = 12288
+TempCacheLimit = 512
+InlineSortThreshold = 134217728
+​MaxParallelWorkers = 0 
+​# Prevent Concurrency Errors 
 LockHashSlots = 65519
 LockMemSize = 30M
-InlineSortThreshold = 16384
-MaxParallelWorkers = 3
 "@
 
-write-host "Checking if Firebird is Installed"
 if (!(Test-Path "C:\Program Files\Firebird")) { 
-    # Updated installer filename to reflect the 4.0.6 64-bit version
-    $installerPath = "$env:TEMP\Firebird-4.0.6-x64.exe"
+    $installerPath = "$env:TEMP\Firebird-4.0.6.3221-0-x64.exe"
     write-host "Firebird is not installed" -ForegroundColor Red
-    
-    write-host "Obtaining Installer"
-    # Download the installer from the new, reliable URL. -ErrorAction Stop ensures immediate failure if download fails.
-    Invoke-WebRequest -Uri $installerUrl -OutFile $installerPath -ErrorAction Stop
-    write-host "Download successful." -ForegroundColor Green
-    
-    write-host "Installing Firebird"
+    Invoke-WebRequest -Uri $installerUrl -OutFile $installerPath
+    write-host "Installing Firebird 64bit V4.0.6 with DataTypeCompatibility = 3.0"
     Start-Process -FilePath $installerPath -ArgumentList "/LANG=en", "/NORESTART", "/VERYSILENT", "/MERGETASKS=UseClassicServerTask,UseServiceTask,CopyFbClientAsGds32Task" -Wait
-    
     write-host "Editing firebird.conf"
-    
-    # Overwrite the entire firebird.conf file with the new content
-    Set-Content "C:\Program Files\Firebird\Firebird_4_0\firebird.conf" -Value $firebirdConfigContent -Force
-    
+    $configPath = "C:\Program Files\Firebird\Firebird_4_0\firebird.conf"
+    Set-Content $configPath -Value $firebirdConfigContent -Force
     write-host "Starting Firebird Service"
     Start-Service -Name "FirebirdServerDefaultInstance"
 }
